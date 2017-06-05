b6dd32b4d1a64e2a60442e9836dfbfe561573d99
Michal Proszek <michal.proszek@gmail.com>
2017-06-05 10:35:28 +0200

    bump version

--- django_stripe/__init__.py
+++ django_stripe/__init__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 __title__ = "Django Stripe"
-__version__ = "0.0.1"
+__version__ = "1.0.0"
 __author__ = "Jacek Ostanski"
 __license__ = "MIT"
 __copyright__ = "Copyright 2017 Arabella"
