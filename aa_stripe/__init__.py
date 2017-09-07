590d6189836d7c77dbc2804c695d98eb654b7b4d
Jacek Ostański <jacek@ivolution.pl>
2017-09-07 18:43:44 -0400

    Update __init__.py

--- aa_stripe/__init__.py
+++ aa_stripe/__init__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 __title__ = "Arabella Stripe"
-__version__ = "0.2.17"
+__version__ = "0.2.18"
 __author__ = "Jacek Ostanski"
 __license__ = "MIT"
 __copyright__ = "Copyright 2017 Arabella"
