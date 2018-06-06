b790bc9fc6ba8d060c473fb3020220157ae72ac4
Michal Proszek <michal.proszek@gmail.com>
2018-06-06 20:55:03 +0200

    [BE] optimize refresh_customers command

--- aa_stripe/management/commands/refresh_customers.py
+++ aa_stripe/management/commands/refresh_customers.py
@@ -1,6 +1,9 @@
 # -*- coding: utf-8 -*-
+import sys
+
 import stripe
 from django.core.management.base import BaseCommand
+from django.utils import timezone
 
 from aa_stripe.models import StripeCustomer
 from aa_stripe.settings import stripe_settings
@@ -11,8 +14,36 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         stripe.api_key = stripe_settings.API_KEY
-        for customer in StripeCustomer.objects.filter(is_active=True, is_created_at_stripe=True):
+        last_customer = None
+        retry_count = 0
+        updated_count = 0
+        start_time = timezone.now()
+        print("Began refreshing customers")
+        while True:
             try:
-                customer.refresh_from_stripe()
+                response = stripe.Customer.list(limit=100, starting_after=last_customer)
             except stripe.StripeError:
-                pass
+                if retry_count > 5:
+                    raise
+                retry_count += 1
+                continue
+            else:
+                retry_count = 0
+
+            for stripe_customer in response["data"]:
+                updated_count += StripeCustomer.objects.filter(stripe_customer_id=stripe_customer["id"]).update(
+                    sources=stripe_customer["sources"], default_source=stripe_customer["default_source"]
+                )
+
+            if not response["has_more"]:
+                break
+
+            sys.stdout.write(".")  # indicate that the command did not hung up
+            sys.stdout.flush()
+            last_customer = response["data"][-1]
+
+        if updated_count:
+            print("\nCustomers updated: {} (took {:2f}s)".format(
+                updated_count, (timezone.now() - start_time).total_seconds()))
+        else:
+            print("No customers were updated.")
