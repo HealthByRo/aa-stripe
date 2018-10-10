4d17c9564de6da81bf6f2b82a67e6df7b512a749
Marcin Kaszynski <>
2018-10-10 21:36:09 +0200

    Add a test for missing migrations.  Fix missing migration.

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,14 +1,13 @@
 # -*- coding: utf-8 -*-
 def pytest_configure():
+    import django
     from django.conf import settings
+    from django.core.management import call_command
 
     settings.configure(
         DEBUG_PROPAGATE_EXCEPTIONS=True,
         DATABASES={
-            "default": {
-                "ENGINE": "django.db.backends.sqlite3",
-                "NAME": ":memory:",
-            }
+            "default": {"ENGINE": "django.db.backends.sqlite3", "NAME": ":memory:"}
         },
         SECRET_KEY="not very secret in tests",
         USE_I18N=True,
@@ -20,12 +19,14 @@ def pytest_configure():
             "django.contrib.admin",
             "django.contrib.sites",
             "rest_framework",
-            "aa_stripe"
+            "aa_stripe",
         ),
         ROOT_URLCONF="aa_stripe.api_urls",
         TESTING=True,
-
         ENV_PREFIX="test-env",
         STRIPE_SETTINGS_API_KEY="apikey",
-        STRIPE_SETTINGS_WEBHOOK_ENDPOINT_SECRET="fake"
+        STRIPE_SETTINGS_WEBHOOK_ENDPOINT_SECRET="fake",
     )
+    django.setup()
+    call_command("migrate")
+    call_command("makemigrations", "--check")
