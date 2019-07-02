ac0bcfbf8cfabab8fe0959407c0413f8c23c38b4
Michal Proszek <michal.proszek@gmail.com>
2019-07-02 09:14:55 +0200

    Improve Stripe Charge admin (#58)
    * Improve Stripe Charge admin
    
    * changes

--- aa_stripe/__init__.py
+++ aa_stripe/__init__.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
 __title__ = "Arabella Stripe"
-__version__ = "0.8.2"
+__version__ = "0.8.3"
 __author__ = "Jacek Ostanski"
 __license__ = "MIT"
-__copyright__ = "Copyright 2017 Arabella"
+__copyright__ = "Copyright 2019 Arabella"
 
 # Version synonym
 VERSION = __version__
