07f8d101e17063ce02684314ca6dcf2e2e0f59c0
Remigiusz Dymecki <remik@users.noreply.github.com>
2019-07-10 13:16:47 +0200

    Update python & Django support (#60)

--- setup.py
+++ setup.py
@@ -75,10 +75,9 @@ setup(
         "Intended Audience :: Developers",
         "Operating System :: OS Independent",
         "Programming Language :: Python",
-        "Programming Language :: Python :: 2.7",
-        "Programming Language :: Python :: 3.4",
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
         "Topic :: Internet :: WWW/HTTP",
     ]
 )
