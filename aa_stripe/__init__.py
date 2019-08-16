cce4ee8c6040f85e7cbd737b35e6f5d4707d8fa2
Remigiusz Dymecki <remigiusz@gmail.com>
2019-08-16 10:36:59 -0400

    Bump version

--- aa_stripe/__init__.py
+++ aa_stripe/__init__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 __title__ = "Ro Stripe"
-__version__ = "0.8.4"
+__version__ = "0.8.5"
 __author__ = "Remigiusz Dymecki"
 __license__ = "MIT"
 __copyright__ = "Copyright 2019 Ro"
