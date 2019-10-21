943157ffb4517bd84199e995d60a04acc9a62e9a
Przemysław Świercz <przemek@ro.co>
2019-10-21 15:39:46 +0200

    Handle (internal) stripe api errors (#69)
    * Add stripe api error handling
    * Cleanup test_charges

--- aa_stripe/models.py
+++ aa_stripe/models.py
@@ -423,6 +423,12 @@ class StripeCharge(StripeBasicModel):
                 self.save()
                 stripe_charge_card_exception.send(sender=StripeCharge, instance=self, exception=e)
                 return  # just exit.
+            except stripe.error.APIError as e:
+                self.is_charged = False
+                self.stripe_response = e.json_body
+                logger.error("Temporary Stripe API error")
+                self.save()
+                return
             except stripe.error.StripeError as e:
                 self.is_charged = False
                 self.stripe_response = e.json_body
@@ -437,7 +443,6 @@ class StripeCharge(StripeBasicModel):
                     pass
                 self.save()
                 raise
-
             self.stripe_charge_id = stripe_charge["id"]
             self.stripe_response = stripe_charge
             self.is_charged = True
