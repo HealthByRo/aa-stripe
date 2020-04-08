dec81d88c63dde3131d88f1cf7f772443ee4a45e
Przemysław Świercz <przemek@ro.co>
2020-04-08 14:52:49 +0200

    Bump version

--- aa_stripe/__init__.py
+++ aa_stripe/__init__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 __title__ = "Ro Stripe"
-__version__ = "0.9.4"
+__version__ = "0.9.5"
 __author__ = "Remigiusz Dymecki"
 __license__ = "MIT"
 __copyright__ = "Copyright 2019 Ro"
